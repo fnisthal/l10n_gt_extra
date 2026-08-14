c7a25aa2d0b00fbd1e90c96570989c07ff983184
fnisthal <fnisthal@gmail.com>
2026-08-14 16:15:56 -0600

    Agregar metodo analiticas() al reporte de Partida para leer analytic_distribution
    Reemplaza el uso de analytic_line_ids (que puede quedar vacio si no se generaron lineas analiticas) por lectura directa de analytic_distribution, soportando distribuciones combinadas entre varias cuentas analiticas.

--- report/reporte_partida.py
+++ report/reporte_partida.py
@@ -7,6 +7,17 @@ class ReportePartida(models.AbstractModel):
     _name = 'report.l10n_gt_extra.reporte_partida'
     _description = 'Partida'
 
+    def analiticas(self, distribucion):
+        nombres = ''
+        if distribucion:
+            cuentas_ids = []
+            for llave in distribucion.keys():
+                for cuenta_id in llave.split(','):
+                    cuentas_ids.append(int(cuenta_id))
+
+            nombres = ', '.join(self.env['account.analytic.account'].browse(cuentas_ids).mapped('display_name'))
+        return nombres
+
     @api.model
     def _get_report_values(self, docids, data=None):
         return self.get_report_values(docids, data)
@@ -21,6 +32,7 @@ class ReportePartida(models.AbstractModel):
             'doc_model': model,
             'docs': docs,
             'current_company_id': self.env.company,
+            'analiticas': self.analiticas,
         }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
